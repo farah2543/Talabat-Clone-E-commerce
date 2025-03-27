bfb0b18dce872bba3493bbb29356c4ebfe070fff
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-03-27 02:21:03 +0200

    5- Specifications

---
+++ E-commerce/Core/Domain/Contracts/Specifications.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public abstract class Specifications <T> where T : class
+    {
+
+        protected Specifications(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+        public Expression<Func<T, bool>>? Criteria { get; }
+
+        // Second Method => Include()
+        public List<Expression<Func<T, object>>> IncludeExpression { get; } = new();
+   
+        // Method to Add Includes
+        protected void AddInclude(Expression<Func<T, object>> expression)
+            => IncludeExpression.Add(expression);
+
+
+
+    }
+}
---
+++ E-commerce/Core/Services/Specifications/ProductWithBrandAndTypeSpecification.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specifications
+{
+    public class ProductWithBrandAndTypeSpecification : Specifications<Product>
+    {
+
+        public ProductWithBrandAndTypeSpecification(int id) : base(prod => prod.Id == id) 
+        {
+            AddInclude(product => product.ProductBrand);
+
+            AddInclude(product => product.ProductType);
+
+
+        }
+
+
+        public ProductWithBrandAndTypeSpecification() : base(null)
+        {
+            AddInclude(product => product.ProductBrand);
+
+            AddInclude(product => product.ProductType);
+
+
+        }
+
+    }
+}
