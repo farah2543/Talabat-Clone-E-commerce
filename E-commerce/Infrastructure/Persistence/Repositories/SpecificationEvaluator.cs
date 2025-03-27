6540d831a52ae705effec799cdcf93b0e57c894b
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-03-27 02:23:47 +0200

    6- Specifications Evaluator

---
+++ E-commerce/Infrastructure/Persistence/Repositories/SpecificationEvaluator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    internal static class SpecificationEvaluator
+    {
+
+        public static IQueryable<T> GetQuery<T>
+            (IQueryable<T> inputQuery, Specifications<T> specifications) where T : class
+        {
+            var query = inputQuery;
+
+            if (specifications.Criteria != null)
+            {
+                query.Where(specifications.Criteria);
+            }
+            //foreach (var item in specifications.IncludeExpression)
+            //{
+            //    query = query.Include(item);
+            //}
+
+            query = specifications.IncludeExpression.
+                Aggregate(query, (curr, includeExpression) => curr.Include(includeExpression));
+
+
+            return query;
+
+
+
+        }
+    }
+}
