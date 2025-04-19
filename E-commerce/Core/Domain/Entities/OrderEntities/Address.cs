1b2772468b2d2247f0ba67c58b9a9b4717782812
farahmohmed <102584837+farah2543@users.noreply.github.com>
2025-04-19 14:10:36 +0200

    1- Order Entities

---
+++ E-commerce/Core/Domain/Entities/OrderEntities/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.OrderEntities
+{
+    public class Address
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string street { get; set; }
+
+     
+    }
+}
---
+++ E-commerce/Core/Domain/Entities/OrderEntities/Order.cs
@@ -0,0 +1,29 @@
+﻿using ShippingAddress = Domain.Entities.OrderEntities.Address;
+
+namespace Domain.Entities.OrderEntities
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; }
+
+        public ShippingAddress Address { get; set; }
+
+        public ICollection<OrderItem> orderItems { get; set; } = new List<OrderItem>();
+
+
+        public OrderPaymentStatus PaymentStatus { get; set; } = OrderPaymentStatus.Pending;
+
+        public DeliveryMethod DeliveryMethod { get; set; }
+
+        public int? DeliveryMethodId { get; set; }
+
+
+        public decimal SubTotal { get; set; }
+
+
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+        public string? PaymentIntentId { get; set; } = string.Empty;
+
+    }
+}
